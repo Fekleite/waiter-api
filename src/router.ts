6687c36c19b5748d16012ee04799fd56f334b0ae
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-06-28 17:37:16 -0300

    feat: setup multer to handle with image uploads

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,33 @@
 import { Router } from 'express';
-
+import multer from 'multer';
+import path from 'node:path';
 import { createCategory } from './app/use-cases/categories/create-category';
 import { listCategories } from './app/use-cases/categories/list-categories';
+import { createProduct } from './app/use-cases/products/create-product';
+import { listProducts } from './app/use-cases/products/list-products';
 
 export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (_, __, callback) => {
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
+    },
+    filename: (_, file, callback) => {
+      const timestamp = Date.now();
+      const filename = `${timestamp}-${file.originalname}`;
+      callback(null, filename);
+    },
+  }),
+});
 
 router.get('/categories', listCategories);
 
 router.post('/categories', createCategory);
 
-router.get('/products', (_, res) => {
-  res.status(200).json([
-    { id: '1', name: 'Pizza' },
-    { id: '2', name: 'Burger' },
-  ]);
-});
+router.get('/products', listProducts);
 
-router.post('/products', (_, res) => {
-  res.status(201).json({ id: '3', name: 'Soda' });
-});
+router.post('/products', upload.single('image'), createProduct);
 
 router.get('/categories/:categoryId/products', (_, res) => {
   res.status(200).json([
