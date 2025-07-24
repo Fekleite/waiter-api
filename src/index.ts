4049653b80066d6dcd2a8a27d6f806e78b714267
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-07-24 12:13:23 -0300

    feat: setup default response headers

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,12 @@ mongoose
 
     const app = express();
 
+    app.use((_, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', '*');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    });
     app.use(
       '/uploads',
       express.static(path.resolve(__dirname, '..', 'uploads')),
